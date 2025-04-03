a6052b3c7925e54361e8f09b345a943e832f68be
PhaelLima-j <raphael.rodrigues.58010@a.fecaf.com.br>
2025-04-03 11:52:31 -0300

    Subindo dataset para postgresql que está no meu container

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 from sqlalchemy import create_engine
+
+df = pd.read_csv('IOT-TEMP.csv', sep=',')
+
+engine = create_engine('postgresql://postgres:RaphaelLim4@localhost:5432/postgres-iot')
+
+df.to_sql("temperature_reading", con=engine, if_exists='replace', index=False)
+
+print("Dados Inseridos")
